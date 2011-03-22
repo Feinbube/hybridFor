f2fc4dfcb1059bda1167608467fbb68b79f9680f
Frank Feinbube <Feinbube@users.noreply.github.com>
2011-03-22 16:57:31 +0000

    further refactorings

---
+++ Hybrid/MsilToOpenCL/Context/AccessPathEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hybrid.MsilToOpenCL.HighLevel
+{
+    public class AccessPathEntry
+    {
+        public ArgumentLocation ArgumentLocation;
+        public Dictionary<System.Reflection.FieldInfo, AccessPathEntry> SubEntries = new Dictionary<System.Reflection.FieldInfo, AccessPathEntry>();
+    }
+}
---
+++ Hybrid/MsilToOpenCL/Context/ILBB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hybrid.MsilToOpenCL.HighLevel
+{
+    public class ILBB
+    {
+        public int? stackCountOnEntry;
+        public int? stackCountOnExit;
+        public int offset;
+        public List<CilInstruction> list = new List<CilInstruction>();
+        public CilInstruction FinalTransfer;
+
+        public ILBB FallThroughTarget;
+    }
+}
