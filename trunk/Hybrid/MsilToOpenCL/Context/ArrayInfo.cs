0f1e0cdf04fb77b80d0f8f761eb90d9229d7fb5e
Frank.Feinbube <Frank.Feinbube@48dd62a5-fcd2-434c-a4d8-0da38a3adfcf>
2011-03-22 16:57:31 +0000

    further refactorings

---
+++ trunk/Hybrid/MsilToOpenCL/Context/ArrayInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hybrid.MsilToOpenCL.HighLevel
+{
+    public class ArrayInfo
+    {
+        private ArgumentLocation m_ArrayArgument;
+        private List<Node> m_ScaleNode;
+        private List<ArgumentLocation> m_ScaleArgument;
+        private int m_DimensionCount;
+
+        public ArrayInfo(ArgumentLocation ArrayArgument)
+        {
+            m_ArrayArgument = ArrayArgument;
+            m_DimensionCount = ArrayArgument.DataType.GetArrayRank();
+
+            m_ScaleNode = new List<Node>(DimensionCount);
+            m_ScaleArgument = new List<ArgumentLocation>(DimensionCount);
+
+            for (int i = 0; i < DimensionCount; i++)
+            {
+                m_ScaleNode.Add(null);
+                m_ScaleArgument.Add(null);
+            }
+        }
+
+        public int DimensionCount { get { return m_DimensionCount; } }
+
+        public List<Node> ScaleNode { get { return m_ScaleNode; } }
+        public List<ArgumentLocation> ScaleArgument { get { return m_ScaleArgument; } }
+    }
+}
---
+++ trunk/Hybrid/MsilToOpenCL/Context/StackState.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hybrid.MsilToOpenCL.HighLevel
+{
+    public class StackState
+    {
+        private List<StackLocation> m_StackLocations;
+        private bool m_Complete;
+
+        public List<StackLocation> StackLocations { get { return m_StackLocations; } }
+        public bool Complete { get { return m_Complete; } set { m_Complete = value; } }
+
+        public StackState(int EntryCount, bool? Complete)
+        {
+            m_StackLocations = new List<StackLocation>(EntryCount);
+            for (int i = 0; i < EntryCount; i++)
+            {
+                m_StackLocations.Add(CreateStackLocation(i, null));
+            }
+
+            m_Complete = (Complete.HasValue ? Complete.Value : (m_StackLocations.Count == 0));
+        }
+
+        public StackState(StackState ex)
+        {
+            m_StackLocations = new List<StackLocation>(ex.StackLocations.Count);
+            foreach (StackLocation Location in ex.StackLocations)
+            {
+                m_StackLocations.Add((StackLocation)Location.Clone());
+            }
+        }
+
+        public static StackLocation CreateStackLocation(int Index, Type DataType)
+        {
+            return new StackLocation(Index, DataType);
+        }
+    }
+}
