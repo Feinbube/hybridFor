756379c8dc331a115a21b0808947a9bdf86347d3
Frank.Feinbube <Frank.Feinbube@48dd62a5-fcd2-434c-a4d8-0da38a3adfcf>
2011-05-11 10:21:16 +0000

    first ideas for a data access attribute

---
+++ trunk/Hybrid/Hybrid/Core/DataAccessAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hybrid.Core
+{
+    // [DataAccess(Access.ReadOnly, Pattern.Linear, Stride=3)]
+    // [DataAccess(Access.ReadOnly, Pattern.Arbitrary)]
+    // [DataAccess(Access.ReadOnly, Pattern.Complex, AffectedPlacesCallback=myCallback)]
+
+    public class DataAccessAttribute : Attribute 
+    { 
+        public enum Pattern { Linear, Complex, Arbitrary }
+        public enum Access { ReadOnly, WriteOnly, ReadWrite }
+        public delegate List<int> AffectedPlacesDelegate(int id);
+        
+        public Access access = Access.ReadWrite;
+        public Pattern pattern = Pattern.Arbitrary;
+
+        public int Stride = 1;
+        public AffectedPlacesDelegate AffectedPlacesCallback;
+
+        public DataAccessAttribute(Access access, Pattern pattern)
+        {
+            this.access = access;
+            this.pattern = pattern;
+        }
+    }
+}
