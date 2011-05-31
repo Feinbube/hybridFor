aa85df832ab95750d460bc31112cabdedec4da15
ingo.jaeckel <ingo.jaeckel@48dd62a5-fcd2-434c-a4d8-0da38a3adfcf>
2011-05-31 10:07:41 +0000

    added TODO into Scheduler and AlgorithmCharacteristics to express how AlgorithmCharacteristics needs to be extended

--- Hybrid/Hybrid/Core/AlgorithmCharacteristics.cs
+++ Hybrid/Hybrid/Core/AlgorithmCharacteristics.cs
@@ -7,6 +7,20 @@ namespace Hybrid
 {
     public class AlgorithmCharacteristics
     {
+        private Action<int> action;
+
+        public AlgorithmCharacteristics()
+        {
+        }
+
+        public AlgorithmCharacteristics(Action<int> action)
+        {
+            // atomic attribute?
+            // double?
+
+            this.action = action;
+        }
+
         public bool UsesDoublePrecisionFloatingPoint { get; set; }
         public bool UsesAtomics { get; set; }
     }
