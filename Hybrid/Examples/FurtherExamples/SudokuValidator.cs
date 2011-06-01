38248218dd0fee87581be2bfd56de776ef336f10
ingo.jaeckel <ingo.jaeckel@48dd62a5-fcd2-434c-a4d8-0da38a3adfcf>
2011-06-01 15:38:49 +0000

    extended SudokuValidator example with missing kernels. Increased size of field from n = 9 to n = 50.

--- Hybrid/Examples/FurtherExamples/SudokuValidator.cs
+++ Hybrid/Examples/FurtherExamples/SudokuValidator.cs
@@ -5,26 +5,21 @@ using System.Text;
 
 namespace Hybrid.Examples
 {
-    public class SudokuValidator: ExampleBase
+    public class SudokuValidator : ExampleBase
     {
         int[] field;
-        int n, shift;
+        int n = 50;
+        int shift = 0;
 
         protected override void scaleAndSetSizes(double sizeX, double sizeY, double sizeZ)
         {
-            this.sizeX = -1; // unused
-            this.sizeY = -1; // unused
-            this.sizeZ = 1; // Used to store the number of rounds
-
-            this.n = 20;
-            this.shift = 0;
+            this.sizeX = this.sizeY = n * n;
+            this.sizeZ = -1; // unused
         }
 
         protected override void setup()
         {
-            field = new int[sizeX * sizeY];
-
-
+            field = new int[n * n * n * n];
         }
 
         protected override void printInput()
@@ -70,13 +65,13 @@ namespace Hybrid.Examples
                 int value = field[x + y * n * n];
 
                 if (value > (n * n) || value <= 0)
-                    invalidFieldIndicator = 1; 
+                    invalidFieldIndicator = 1;
             });
 
             return invalidFieldIndicator != 0;
         }
 
-        private bool containsInvalidRow()
+        private bool containsInvalidRowKernel()
         {
             int invalidFieldIndicator = 0;
 
@@ -85,9 +80,68 @@ namespace Hybrid.Examples
                 int x = id / (n * n);
                 int y = id % (n * n);
 
-               for (int x2 = (x+1); x2 < n*n; x2++)						
-		            if(field[(x)+((y)*n*n)] == field[x2+y*n*n])	 		
-			            invalidFieldIndicator = 2; // return true;	
+                for (int x2 = (x + 1); x2 < n * n; x2++)
+                    if (field[x + y * n * n] == field[x2 + y * n * n])
+                        invalidFieldIndicator = 2; // return true;	
+            });
+
+            return invalidFieldIndicator != 0;
+        }
+
+        private bool containsInvalidSubfieldKernel()
+        {
+            int invalidFieldIndicator = 0;
+
+            Parallel.For(0, n * n, delegate(int threadId)
+            {
+                int n2 = n * n;
+
+                // for (int subfield = 0; subfield < n*n; subfield++)           
+                int subfield = threadId / (n2);
+
+                // for (int cell = 0; cell < n*n-1; cell++)                     
+                int cell = threadId % (n2);
+                if (cell >= n2 - 1)
+                    return;
+
+                for (int cell2 = cell + 1; cell2 < n2; cell2++)
+                {
+                    int subfield_t1 = (subfield % n) * n;
+                    int subfield_t2 = (subfield / n) * n;
+
+                    int x1 = subfield_t1 + (cell % n);
+                    int y1 = subfield_t2 + (cell / n);
+
+                    int x2 = subfield_t1 + (cell2 % n);
+                    int y2 = subfield_t2 + (cell2 / n);
+
+                    if (field[x1 + y1 * n2] == field[x2 + y2 * n2])
+                        invalidFieldIndicator = 4; // return true;				
+                }
+            });
+
+            return invalidFieldIndicator != 0;
+        }
+
+        private bool containsInvalidColumnKernel()
+        {
+            int invalidFieldIndicator = 0;
+
+            Parallel.For(0, n * n, delegate(int threadId)
+            {
+                int n2 = n * n;
+
+                // for (int y = 0; y < n2; y++)                             
+                int y = threadId / (n2);
+
+                // for (int x = 0; x < n2-1; x++)                            
+                int x = threadId % (n2);
+                if (x >= n2 - 1)
+                    return;
+
+                for (int y2 = y + 1; y2 < n2; y2++)
+                    if (field[x + y * n2] == field[x + y2 * n2])
+                        invalidFieldIndicator = 3; // return true;		
             });
 
             return invalidFieldIndicator != 0;
@@ -95,17 +149,10 @@ namespace Hybrid.Examples
 
         private bool isValidField()
         {
-            bool containsInvalidNumberKernel = false;
-            bool containsInvalidRowKernel = false;
-            bool containsInvalidColumnKernel = false;
-            bool containsInvalidSubfieldKernel = false;
-
-            // TODO calculate each boolean values in parallel because the field is big.
-
-            bool invalid = containsInvalidNumberKernel
-                || containsInvalidRowKernel
-                || containsInvalidColumnKernel
-                || containsInvalidSubfieldKernel;
+            bool invalid = containsInvalidNumberKernel()
+                || containsInvalidRowKernel()
+                || containsInvalidColumnKernel()
+                || containsInvalidSubfieldKernel();
 
             return !invalid;
         }
@@ -117,7 +164,7 @@ namespace Hybrid.Examples
 
         protected override bool isValid()
         {
-            throw new NotImplementedException();
+            return false;
         }
     }
 }
