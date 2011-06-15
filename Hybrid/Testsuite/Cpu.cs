262b91cc41addd93c84878498ec2836992679234
Frank Feinbube <Feinbube@users.noreply.github.com>
2011-06-15 11:12:00 +0000

    some examples are "Functionality examples" instead of "Further Examples" now. testing is deduplicated now. (use column view and chose "class name" to get information like "all gpu tests failed")

---
+++ Hybrid/Testsuite/Cpu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Hybrid;
+using Hybrid.Examples;
+using Hybrid.Examples.Upcrc2010.MatrixMultiplication;
+using Hybrid.Examples.CudaByExample;
+using Hybrid.Examples.Upcrc2010;
+
+namespace Hybrid.Testsuite
+{
+    [TestClass]
+    public class Cpu : ExampleTestBase
+    {
+        override protected void testExample(ExampleBase example)
+        {
+            example.ExecuteOn = Execute.OnAllCpus;
+
+            if(example.Run(0.1, 0.1, 0.1, false, 5, 2, null) < 0 )
+                throw new Exception("Invalid result for " + example.GetType());
+        }
+    }
+}
---
+++ Hybrid/Testsuite/MultiGpu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Hybrid;
+using Hybrid.Examples;
+using Hybrid.Examples.Upcrc2010.MatrixMultiplication;
+using Hybrid.Examples.CudaByExample;
+using Hybrid.Examples.Upcrc2010;
+
+namespace Hybrid.Testsuite
+{
+    [TestClass]
+    public class MultiGpu : ExampleTestBase
+    {
+        override protected void testExample(ExampleBase example)
+        {
+            example.ExecuteOn = Execute.OnAllGpus;
+
+            if (example.Run(0.1, 0.1, 0.1, false, 5, 2, null) < 0)
+                throw new Exception("Invalid result for " + example.GetType());
+
+            Parallel.ReInitialize();
+        }
+    }
+}
