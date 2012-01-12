4a16b8c14edb320cefdb18b2afcd2e75e82a3ef3
Jan-Arne.Sobania <Jan-Arne.Sobania@48dd62a5-fcd2-434c-a4d8-0da38a3adfcf>
2012-01-12 13:12:03 +0000

    fix: CIL Opcode ldsfld

--- trunk/Hybrid/MsilToOpenCL/Instructions/CilLoadFieldInstruction.cs
+++ trunk/Hybrid/MsilToOpenCL/Instructions/CilLoadFieldInstruction.cs
@@ -10,6 +10,7 @@ namespace Hybrid.MsilToOpenCL.Instructions
     public class CilLoadFieldInstruction : CilInstruction
     {
         private FieldInfo m_FieldInfo;
+        private bool m_WithoutThis;
 
         public static CilInstruction Create(OpCode Opcode, byte[] IL, int Offset, int NextOffset, MethodBase ParentMethodBase)
         {
@@ -21,11 +22,16 @@ namespace Hybrid.MsilToOpenCL.Instructions
         private CilLoadFieldInstruction(OpCode Opcode, int Offset, FieldInfo FieldInfo)
             : base(Opcode, Offset)
         {
-            if (Opcode != OpCodes.Ldfld)
+            if (Opcode != OpCodes.Ldfld && Opcode != OpCodes.Ldsfld)
             {
                 throw new ArgumentException("Opcode");
             }
             m_FieldInfo = FieldInfo;
+            m_WithoutThis = (Opcode == OpCodes.Ldsfld);
+            if (m_WithoutThis && !m_FieldInfo.IsStatic)
+            {
+                throw new InvalidOperationException("Attempted 'ldsfld' on non-static field.");
+            }
         }
 
         public FieldInfo FieldInfo
@@ -44,7 +50,7 @@ namespace Hybrid.MsilToOpenCL.Instructions
         public override void WriteCode(System.IO.TextWriter writer, int indent, int CurStack)
         {
             WriteInstHeader(writer, indent);
-            writer.WriteLine("{0}{1} = ({2}).__field(\"{3}\");", IndentString(indent), StackName(CurStack), StackName(CurStack), m_FieldInfo.DeclaringType.FullName + "::" + m_FieldInfo.Name);
+            writer.WriteLine("{0}{1} = ({2}).__field(\"{3}\");", IndentString(indent), StackName(CurStack), m_WithoutThis ? "<<static>>" : StackName(CurStack), m_FieldInfo.DeclaringType.FullName + "::" + m_FieldInfo.Name);
         }
 
         public override List<HighLevel.Instruction> GetHighLevel(HighLevel.HlGraph Context)
@@ -61,7 +67,7 @@ namespace Hybrid.MsilToOpenCL.Instructions
                 Argument = new HighLevel.InstanceFieldNode(Context.ReadStackLocationNode(Context.StackPointer), FieldInfo);
             }
 
-            List.Add(new HighLevel.AssignmentInstruction(Context.DefineStackLocationNode(Context.StackPointer), Argument));
+            List.Add(new HighLevel.AssignmentInstruction(Context.DefineStackLocationNode(m_WithoutThis ? Context.StackPointer + 1 : Context.StackPointer), Argument));
             return List;
         }
     }
