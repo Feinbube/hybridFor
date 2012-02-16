452161fa331f5acd3775dca21e5ba5438795df6a
ralf.diestelkaemper <ralf.diestelkaemper@48dd62a5-fcd2-434c-a4d8-0da38a3adfcf>
2012-02-16 15:03:42 +0000

    New Functionality Tests
    There are two new functionality tests added to check the random generator and the atomic add function.

---
+++ Hybrid/Examples/Functionality/AtomicExample.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hybrid.Examples.Functionality
+{
+    public class AtomicExample : ExampleBase
+    {
+        int[] a;
+        int result;
+        protected override void setup()
+        {
+            if (sizeX > Int32.MaxValue || sizeX < 0)
+                sizeX = Int32.MaxValue;
+            a = new int[sizeX];
+            for (int x = 0; x < sizeX; x++)
+            {
+                a[x] = 1;
+            }
+            result = 0;
+        }
+
+        protected override void printInput()
+        {
+            printField(a, sizeX);
+        }
+
+        protected override void algorithm()
+        {
+            result = 0;
+            Parallel.For(ExecuteOn,0,sizeX,delegate(int x){
+                Atomic.Add(ref result, a[x]);
+            });
+        }
+
+        protected override void printResult()
+        {
+            Console.WriteLine(result);
+        }
+
+
+        protected override void cleanup()
+        {
+            a = null;
+        }
+
+        protected override bool isValid()
+        {
+            if (result == sizeX)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
